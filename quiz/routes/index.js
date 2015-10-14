f54e08eb8793ea2c825d33bff4ab925d80f16d8f
Juan Jose Moliner Garcia <mrmolihellin@gmail.com>
2015-10-14 08:15:32 +0200

    Autores con array

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller')
+var autoresController = require('../controllers/autores_controller')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+//router.get('/autores', function(req, res, next) {
+//res.render ('autores/autores');
+//});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/autores', autoresController.autores);
+
 
 module.exports = router;
