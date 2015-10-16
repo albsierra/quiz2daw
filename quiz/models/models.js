7aa98566ac099c37d7b94dafc86440ecc395ef64
leninnaranjo55 <leninnaranjo55@gmail.com>
2015-10-16 13:35:27 +0200

    Integracion del modelo sequelize

---
+++ quiz/models/models.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var path = require('path');
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+//Usar BBDD SQLite:
+var sequelize = new Sequelize(null,null,null,
+            {dialect: "sqlite", storage: "quiz.sqlite" }
+);
+//Importar la deifinicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,"quiz"));
+exports.Quiz = Quiz //exportar la definicion de la tabla quiz
+//sequelize.sync()crea e inicializaba tabla de preguntas en BD
+sequelize.sync().then(function(){
+    // then(..)ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function(count){
+        if(count === 0){ //la tabla se inicializa solo si esta vacia
+            Quiz.create({pregunta: 'Capital de Italia',
+                         respuesta: 'Roma'})
+                             .then(function(){console.log('Base de datos inicializada')});
+        };
+    });
+});
