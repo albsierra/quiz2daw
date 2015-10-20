082d8c34a624fcbd6f4c59b9f00f8158319d102d
leninnaranjo55 <leninnaranjo55@gmail.com>
2015-10-20 17:02:22 +0200

    new preguntas form

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -1,30 +1,5 @@
 
 var models = require('../models/models.js');
-// GET /quizes/question
-exports.show = function(req, res) {
-    models.Quiz.findById(req.params.quizId).then(function(quiz){
-    res.render('quizes/show', {quiz: req.quiz});
-})
-};
-
-// GET /quizes/answer
-exports.answer = function(req, res) {
-    var resultado= 'Incorrecto';
-        if(req.query.respuesta === req.quiz.respuesta){
-            resultado='Correcto';
-    } else {
-        res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado});
-    }
-    
-};
-
-// GET/quizes
-exports.index=function(req, res){
-    models.Quiz.findAll().then(function(quizes){
-        res.render('quizes/index.ejs',{quizes:quizes});
-    })
-};
-
 //Autoload - factoriza el codigo si ruta incluye :quizId
 
 exports.load=function(req, res, next, quizId){
@@ -37,3 +12,42 @@ exports.load=function(req, res, next, quizId){
                     ).catch(function(error){next(error);});
 };
 
+
+// GET /quizes/question
+exports.show = function(req, res) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz){
+    res.render('quizes/show', {quiz: req.quiz});
+})
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+    var resultado= 'Incorrecto';
+    if(req.query.respuesta === req.quiz.respuesta){
+            resultado='Correcto';
+    }
+    res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado});
+};
+
+// GET/quizes
+exports.index=function(req, res){
+    models.Quiz.findAll().then(function(quizes){
+        res.render('quizes/index.ejs',{quizes:quizes});
+    })
+};
+
+//GET /quizes/new
+exports.new = function(req, res){
+    var quiz = models.Quiz.build(
+            {pregunta:'Pregunta',respuesta:'Respuesta'});
+    res.render('quizes/new',{quiz:quiz});
+};
+
+//POST/quizes/create
+exports.create= function(req ,res){
+    var quiz = models.Quiz.build(req.body.quiz);
+    //gurda en la bd los campos de preguntas y respuetas de quiz
+    quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+        res.redirect('/quizes');
+    })
+};
