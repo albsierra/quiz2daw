279ba4cbc0edbdc0ac25fd88dee30f588f392faa
nicocanovas <nicocanovas7@gmail.com>
2015-10-23 02:12:56 +0200

    numeracion de preguntas

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -13,8 +13,9 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index', {quizes: quizes});
+	models.Quiz.findAll().then(
+                function(quizes) {
+    res.render('quizes/index.ejs', {quizes: quizes});
 });
 }
 
@@ -58,4 +59,28 @@ exports.create = function(req, res) {
 			}
 		}
 	);
+};
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+    var quiz = req.quiz; //autoload de instancia de quiz
+    res.render('quizes/edit', {quiz: quiz});
+};
+
+exports.update = function(req, res) {
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    
+    req.quiz
+            .validate()
+            .then(
+            function(err){
+                if(err){
+                    res.render('quizes/edit',{quiz: req.quiz, errors: err.errors});
+                }else{
+                    req.quiz
+                            .save({fields:["pregunta","respuesta"]})
+                            .then(function(){res.redirect('/quizes');});
+                }
+            }
+        );
 };
