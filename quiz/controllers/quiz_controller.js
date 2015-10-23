8a51d4a3ac25dadbf92db9480b14ef35f48a7bb1
osmar villca <osmarvillcachoquedaw@gmail.com>
2015-10-23 12:00:12 +0200

    borrar más numeracion de preguntas

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -25,10 +25,12 @@ exports.answer = function(req, res) {
 };
 //GET/quizes
 exports.index=function(req, res){
-    models.Quiz.findAll().then(function(quizes){
-        res.render('quizes/index.ejs',{quizes:quizes});
-    })
-}
+    models.Quiz.findAll().then(
+            function(quizes){
+        res.render('quizes/index.ejs',{quizes : quizes});
+    }
+    ).catch(function(error){next(error)});
+};
 
 //Autoload-factoriza el codigo si la ruta incluye :quizId
 
@@ -37,7 +39,7 @@ exports.new = function(req,res){
     var quiz=models.Quiz.build(
             {pregunta: "Pregunta", respuesta: "Respuesta"}
     );
-    res.render('quizes/new', {quiz: quiz});
+    res.render('quizes/new', {quiz : quiz});
 };
 //POST /quizes/create
 exports.create=function(req,res){
@@ -46,4 +48,33 @@ exports.create=function(req,res){
     quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
         res.redirect('/quizes');
     })  //Redireccion HTTP (URL relativo) lista de preguntas
+};
+exports.edit=function(req,res){
+    var quiz=req.quiz;//autoload de instancia de quiz
+    res.render('quizes/edit', {quiz : quiz});
+};
+
+exports.update=function(req,res){
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    
+    req.quiz
+            .validate()
+            .then(
+            function(err){
+                if(err){
+                res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+            }else  {
+                req.quiz
+                        .save({fields:["pregunta", "respuesta"]})
+                        .then(function(){res.redirect('/quizes');});
+            }
+        }
+    );
+};
+//DELETE /quizes/:id
+exports.destroy = function(req,res){
+    req.quiz.destroy().then(function(){
+        res.redirect('/quizes');
+    }).catch(function(error){next(error)});
 };
