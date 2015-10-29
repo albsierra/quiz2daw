2e4b4458bd368178baeb52372989cad336959658
nicocanovas <nicocanovas7@gmail.com>
2015-10-29 05:13:42 +0100

    publicacion de commentarios

--- quiz/models/comment.js
+++ quiz/models/comment.js
@@ -4,7 +4,11 @@ module.exports = function(sequelize, DataTypes){
     { texto: {
             type: DataTypes.STRING,
             validate: { notEmpty: {msg: "-> Falte Cmonetario"}}
-    }
+    },
+	publicado: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false
+	}
 }
             );
 }
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new); //formulario login
@@ -33,6 +34,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 
 module.exports = router;
