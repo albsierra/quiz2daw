99a3e0a08628df7c7b7b54d3f327d10e084e6c62
nirtika <menirtika@gmail.com>
2015-10-29 14:29:58 +0100

    Moderacion de Comentarios

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);  //autoload :commentId
 
 router.get('/login',  sessionController.new);
 router.post('/login', sessionController.create);
@@ -31,5 +32,6 @@ router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, 	quizCo
 
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',	commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
